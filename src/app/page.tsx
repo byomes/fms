562f00d2964df47ddc744e1fce3e2af18eedbe85
Bill Yomes <bill.yomes@gmail.com>
2026-06-30 12:55:26 -0400

    refactor: restore footer signup strip; remove mid-page HomeEmailSignup from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import type { Metadata } from 'next'
-import HomeEmailSignup from '@/components/HomeEmailSignup'
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -105,8 +104,6 @@ export default function HomePage() {
         </div>
       </section>
 
-      {/* Email Signup */}
-      <HomeEmailSignup />
     </>
   )
 }
